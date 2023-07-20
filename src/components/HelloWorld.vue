41110cde285a8ee11fa8a8b2e60cb52871335f71
DHoper <gxgemini777@gmail.com>
2023-07-20 14:09:38 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div id="message"></div>
+    <button @click="sendToParent">发送</button>
+  </div>
+</template>
+<script>
+export default {
+  mounted () {
+    window.onload = () => {
+      // 子页面接收消息
+      window.addEventListener('message', function (e) {
+
+        window.document.getElementById('message').innerHTML = e.data.name
+        
+      }, false)
+    }
+  },
+  methods: {
+    // 子页面发送消息
+    sendToParent () {
+      window.parent.postMessage({
+        msg: 'hideselfService',
+        key: '123456'
+      }, '*')
+    }
+  }
+}
+</script>

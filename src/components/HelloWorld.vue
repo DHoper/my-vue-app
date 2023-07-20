c7b09af1d8dbfafe35c3d957e2d5a47d0d2fc6ec
DHoper <gxgemini777@gmail.com>
2023-07-20 15:55:35 +0800

    dd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,11 +25,11 @@ export default {
   mounted() {
     // alert(777,this.data);
       window.addEventListener('message', function (e) {
-        this.data = e.data;
-        let a = JSON.stringify(e.data);
-        console.log(e.data, 755);
-        window.document.getElementById('message').innerHTML = e.data;
-       alert(777,a);
+        this.data = e.data.title;
+        // let a = JSON.stringify(e.data);
+        console.log(this.data, 755);
+        window.document.getElementById('message').innerHTML = this.data;
+        alert(777,a);
 
       }, false)
     

d2050e9d2d8afd9b14136fb4b18b3e630a5f34da
DHoper <gxgemini777@gmail.com>
2023-07-20 15:51:42 +0800

    aa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>01</h1>
+    <h1>0w</h1>
     <div>{{data}}</div>
     <div id="message"></div>
     <button @click="getData">取得資料</button>
@@ -26,7 +26,7 @@ export default {
     // alert(777,this.data);
       window.addEventListener('message', function (e) {
         this.data = e.data;
-        a = JSON.stringify(e.data);
+        let a = JSON.stringify(e.data);
         // window.document.getElementById('message').innerHTML = e.data.name;
        alert(777,a);
 
